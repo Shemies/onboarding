d8d6105a5d3581aee6377969b355a5da5e7e6a38
Shemies <abdulrahman.shemis@bokra.com>
2025-02-09 13:23:32 +0200

    admin dashboard pagination

--- tailwind/js.js
+++ tailwind/js.js
@@ -1,25 +1,21 @@
-const products = [
-    { name: "Product 1", price: "$10", img: "productsPic/1.png" },
-    { name: "Product 2", price: "$15", img: "productsPic/2.png" },
-    { name: "Product 3", price: "$20", img: "productsPic/3.png" },
-    { name: "Product 4", price: "$25", img: "productsPic/4.png" },
-    { name: "Product 5", price: "$30", img: "productsPic/5.png" },
-    { name: "Product 6", price: "$35", img: "productsPic/6.png" },
-    { name: "Product 11", price: "$55", img: "productsPic/1.png" },
-    { name: "Product 22", price: "$42", img: "productsPic/2.png" },
-    { name: "Product 33", price: "$32", img: "productsPic/3.png" },
-    { name: "Product 44", price: "$28", img: "productsPic/4.png" },
-    { name: "Product 55", price: "$19", img: "productsPic/5.png" },
-    { name: "Product 66", price: "$4", img: "productsPic/6.png" }
-];
-
+const products = JSON.parse(localStorage.getItem("products")) || [];
 
 let isAscending = true; //to check the sortstatus
+let filteredProducts = [...products]; // This will store the current filtered/sorted list
+const productsPerPage = 8;
+let currentPage = 1;
+const container = document.getElementById("product-container");
+const pageDiv = document.getElementById("pagintation");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
-function displayProducts(productsToDisplay) {
-    const container = document.getElementById("product-container");
+function displayProducts() {
     container.innerHTML = ""; // Clear existing products
-    productsToDisplay.forEach(product => {
+    const start = (currentPage - 1) * productsPerPage;
+    const end = start + productsPerPage;
+    JSON.parse(localStorage.getItem("products")) || [];
+    const productsinPage = filteredProducts.slice(start , end);
+    productsinPage.forEach(product => {
         const productCard = document.createElement("a");
         productCard.href = `product.html?name=${encodeURIComponent(product.name)}&price=${product.price}&img=${encodeURIComponent(product.img)}`;
         productCard.classList.add("flex", "flex-col" , "items-center" , "rounded-xl" , "p-6" , "text-center" , "m-1" , "transition-transform" , "duration-300" , "ease-in-out" , "hover:scale-105" , "shadow-md" , "bg-white" , "justify-center");
@@ -30,18 +26,57 @@ function displayProducts(productsToDisplay) {
         `;
         container.appendChild(productCard);
     });
+    updatePagination();
+}
+
+function updatePagination() {
+    pageDiv.innerHTML = "";
+    const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageBtn = document.createElement("button");
+        pageBtn.className = `rounded-lg px-4 cursor-pointer py-2 m-2 ${i === currentPage ? "bg-white text-black" : "bg-gray-500 hover:bg-gray-700 text-white"}`;
+        pageBtn.textContent = i;
+        pageBtn.addEventListener("click", () => {
+            currentPage = i;
+            displayProducts();
+        });
+        pageDiv.appendChild(pageBtn);
+    }
+
+    prevBtn.disabled = currentPage === 1;
+    nextBtn.disabled = currentPage === totalPages;
+}
+
+if (prevBtn) {
+    prevBtn.addEventListener("click" , () => {
+        if (currentPage > 1){
+            currentPage--;
+            displayProducts();
+        }
+    });
+}
+if (nextBtn) {
+    nextBtn.addEventListener("click" , () => {
+        if (currentPage < Math.ceil(filteredProducts.length / productsPerPage)){
+            currentPage++;
+            displayProducts();
+        }
+    });
 }
 
 function filterProducts() {
     const searchInput = document.getElementById("search").value.toLowerCase();
-    const filteredProducts = products.filter(product => 
+    filteredProducts = products.filter(product => 
         product.name.toLowerCase().includes(searchInput) //searches the products that include the input
     );
-    displayProducts(filteredProducts); // display filtered products using display fn
+    currentPage = 1;
+    displayProducts(); // display filtered products using display fn
+    updatePagination();
 }
 
 function sortProductsByPrice() {
-    const sortedProducts = [...products].sort((a, b) => { //newarray containsproducts and arrange the products in a specific order
+    filteredProducts.sort((a, b) => { //newarray containsproducts and arrange the products in a specific order
         const priceA = parseFloat(a.price.replace('$', '')); //removes the dollar sign ($) and converts the string to a floating-point number
         const priceB = parseFloat(b.price.replace('$', ''));
         
@@ -54,10 +89,13 @@ function sortProductsByPrice() {
     
     // Toggle sorting order for next click
     isAscending = !isAscending;
-    
-    displayProducts(sortedProducts); // Display sorted products
+    currentPage = 1;
+    displayProducts(); // Display sorted products
+    updatePagination();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    displayProducts(products); // Display all products when the page loads
+    displayProducts(); // Display all products when the page loads
 });
+
+
